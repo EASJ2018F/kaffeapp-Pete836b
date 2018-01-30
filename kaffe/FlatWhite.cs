4b65993c0796d73f2d134857acbd2298d1ce9b7f
Peter Clausen <pete836b@edu.easj.dk>
2018-01-30 14:20:43 +0100

    done

---
+++ kaffe/FlatWhite.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kaffe
+{
+     public class FlatWhite : Kaffe, Imælk
+    {
+        /// <summary>
+        /// methode for at override prisen.
+        /// </summary>
+        /// <returns></returns>
+        public override int Pris()
+        {
+            return 45;
+        }
+        /// <summary>
+        /// Methode for milliliter mælken. 
+        /// </summary>
+        /// <returns></returns>
+ public int MlMælk()
+        {
+            return 160;
+        }
+
+        /// <summary>
+        /// Methode for Override styrken af Kaffen.
+        /// </summary>
+        /// <returns></returns>
+        public override string Styrke()
+        {
+            return "Mild";
+        }
+    }
+}
---
+++ kaffeTests/FlatWhiteTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using kaffe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace kaffeTests
+
+{
+    [TestClass()]
+
+     public class FlatWhiteTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+            // Arrange
+            var fw = new FlatWhite();
+            // Act
+            int result = fw.Pris(); 
+            // Assert
+            Assert.AreEqual(45 , result);
+        }
+
+
+        [TestMethod()]
+        public void TestMlMælk()
+        {
+            // Arrange 
+            var fw = new FlatWhite();
+            // Act
+            int result = fw.MlMælk();
+            // Assert
+            Assert.AreEqual(160 , result);
+        }
+
+        [TestMethod()]
+        public void TestStryke()
+        {
+            var fw = new FlatWhite();
+            string result = fw.Styrke();
+            Assert.AreEqual("Mild", result);
+        }
+    }
+}
